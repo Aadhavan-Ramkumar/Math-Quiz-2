d992142b1ba0ecb42eec54f709054e026c03889e
Aadhavan-Ramkumar <74062499+Aadhavan-Ramkumar@users.noreply.github.com>
2021-04-22 12:56:25 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+function AddUser() {
+    Player1 = document.getElementById("Username1").value;
+    Player2 = document.getElementById("Username2").value;
+    localStorage.setItem("Player1", Player1);
+    localStorage.setItem("Player2", Player2);
+}
